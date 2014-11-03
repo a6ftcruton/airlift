8fb1177beb5d5694330c0ced007cd253a76d86fd
Jessica Goulding <jgoulding2@gmail.com>
2014-11-03 14:09:28 -0700

    refactoring ability and css changes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,24 +9,20 @@ class Ability
       can :manage, :all
     elsif user.is? :store_admin
       can :manage, Item, organization_id: user.organization.id
-      can :maange, Category
-      can :create, User
+      can :manage, Category
       can :manage, User, manager_id: user.id
       cannot :manage, User, self_managed: true
       can :manage, User, id: user.id
     elsif user.is? :user
-      can :read, Item
-      can :read, Category
       can :create, Order
       can :read, Order
       can :manage, User, id: user.id
       can :manage, Review
-    else
-      can :read, Item
-      can :read, Category
-      can :read, Review
-      can :create, User
     end
+    can :read, Item
+    can :read, Category
+    can :read, Review
+    can :create, User
   end
 
 end
